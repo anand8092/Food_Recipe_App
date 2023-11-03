267ed1711a43a2d459ea7bada2ad110cd0ab5712
anand8092 <ad.ananddevkar7875@gmail.com>
2023-11-03 11:05:31 +0530

    Second Commit

---
+++ app/src/main/java/com/example/foodrecipeapp/Activity/IntroActivity.java
@@ -0,0 +1,23 @@
+package com.example.foodrecipeapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.foodrecipeapp.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        ConstraintLayout startBtn=findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(view -> startActivity(new Intent(IntroActivity.this, MainActivity.class)));
+
+    }
+}
